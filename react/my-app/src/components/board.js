3fb7cab100253053b42fa98b0dd407a538c903fc
Nicolas Salvatierra <rodrigo.navarro@wolox.com.ar>
2018-07-02 15:02:12 -0300

    Pr Fix

---
+++ react/my-app/src/components/board.js
@@ -0,0 +1,44 @@
+import React, { Fragment } from 'react';
+
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
+
+class Board extends React.Component {
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
+      />
+    );
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Board;
--- react/my-app/src/tictactoe.js
+++ react/my-app/src/tictactoe.js
@@ -1,47 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-
-
-function Square(props) {
-  return (
-    <button className="square" onClick={props.onClick}>
-      {props.value}
-    </button>
-  );
-}
-
-class Board extends React.Component {
-  renderSquare(i) {
-    return (
-      <Square
-        value={this.props.squares[i]}
-        onClick={() => this.props.onClick(i)}
-      />
-    );
-  }
-
-  render() {
-    return (
-      <React.Fragment>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </React.Fragment>
-    );
-  }
-}
+import Board from './components/board';
 
 class Game extends React.Component {
   constructor(props) {
@@ -128,7 +87,6 @@ Game.propTypes = {
   stepNumber: PropTypes.number,
   xIsNext: PropTypes.bool
 };
-
 
 export default Game;
 
