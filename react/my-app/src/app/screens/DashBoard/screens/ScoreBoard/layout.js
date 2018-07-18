d81bd69b6d952592e9a16d18185c30ea85c2d35c
Nicolas Salvatierra <rodrigo.navarro@wolox.com.ar>
2018-07-18 13:18:48 -0300

    Score is seen

--- react/my-app/src/app/screens/DashBoard/screens/ScoreBoard/layout.js
+++ react/my-app/src/app/screens/DashBoard/screens/ScoreBoard/layout.js
@@ -9,24 +9,15 @@ function Board({ x, o }) {
           <div className="x-team flexbox-items">
             <div className="x-team logo" />
           </div>
-          <div className="flexbox-items">
-            <div id="time-of-match" />
-            <div id="date-of-match" />
-            <br />
-            <div id="vs">
-              <div className="circle" />
-              <hr id="vs-line" />
-              <div className="circle" />
-            </div>
-            <div className="o-team flexbox-items">
-              <div className="o-team logo" />
-            </div>
-          </div>
           <div id="score-container">
             <div className="x-team score">{x}</div>
             <div className="o-team score">{o}</div>
           </div>
-          <hr id="bottom-devider" />
+          <div className="flexbox-items">
+            <div className="o-team flexbox-items">
+              <div className="o-team logo" />
+            </div>
+          </div>
         </div>
       </div>
     </div>
