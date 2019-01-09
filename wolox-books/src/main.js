32b09e1a42ac30278975c1ece286a8a5efd6d7f8
Nicolas Salvatierra <rodrigo.navarro@wolox.com.ar>
2019-01-09 14:24:02 -0300

    Added i18n to register

--- wolox-books/src/main.js
+++ wolox-books/src/main.js
@@ -2,10 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueI18n from 'vue-i18n'
+import { Register as messages } from './views/Home/i18n'
+
+Vue.use(VueI18n)
+
+const i18n = new VueI18n({
+  locale: 'en',
+  messages // set locale messages
+})
 
 Vue.config.productionTip = false
 
 new Vue({
+  i18n,
   router,
   store,
   render: h => h(App)
---
+++ wolox-books/src/views/Home/index.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="register-form" class="home">
+    <form class="register-form" @submit.prevent="onSubmit">
+      <label for="firstName" class="label">{{ $t("register.firstName") }}</label>
+      <input type="text" name="firstName" id="firstName" v-model="firstName">
+      <label for="lastName" class="label">{{ $t("register.lastName") }}</label>
+      <input type="text" name="lastName" id="lastName" v-model="lastName">
+      <label for="email" class="label">{{ $t("register.email") }}</label>
+      <input type="text" name="email" id="email" v-model="email">
+      <label for="password" class="label">{{ $t("register.password") }}</label>
+      <input type="password" name="password" id="password" v-model="password">
+      <button>{{ $t("register.signUp") }}</button>
+    </form>
+    <a href="/">{{ $t("register.logIn") }}</a>
+  </div>
+</template>
+
+<script>
+
+module.exports = {
+  data () {
+    return {
+      firstName: null,
+      lastName: null,
+      email: null,
+      password: null
+    }
+  },
+  methods: {
+    onSubmit () {
+      const data = {
+        'first_name': this.firstName,
+        'last_name': this.lastName,
+        'email': this.email,
+        'password': this.password
+      }
+      console.log(data)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.home {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+}
+.register-form {
+  display: flex;
+  flex-direction: column;
+  width: 300px;
+  align-self: center;
+}
+.label {
+  text-align: left;
+}
+</style>
