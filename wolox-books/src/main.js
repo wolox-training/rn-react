7eb664e1d95f0465b8ef2e001ee67649f8d804da
Nicolas Salvatierra <rodrigo.navarro@wolox.com.ar>
2019-01-10 14:00:47 -0300

    Pr fix

--- wolox-books/src/main.js
+++ wolox-books/src/main.js
@@ -1,22 +1,22 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import VueI18n from 'vue-i18n'
-import { Register as messages } from './views/Home/i18n'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import VueI18n from "vue-i18n";
+import { Register as messages } from "./config/i18n";
 
-Vue.use(VueI18n)
+Vue.use(VueI18n);
 
 const i18n = new VueI18n({
-  locale: 'en',
+  locale: "en",
   messages // set locale messages
-})
+});
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   i18n,
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
