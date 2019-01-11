a6f5c6af3e1eaee6078e2da8e079e3d74d81fef7
Nicolas Salvatierra <rodrigo.navarro@wolox.com.ar>
2019-01-11 15:18:14 -0300

    Pr fix

--- wolox-books/src/router.js
+++ wolox-books/src/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Register'
+import Register from './views/Register'
 
 Vue.use(Router)
 
@@ -9,7 +9,7 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: Register
     }
   ]
 })
