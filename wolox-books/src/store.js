867419b7e03ca55435735ad82788cdd8b519c192
NAVI <40175251+rlajous@users.noreply.github.com>
2019-01-17 11:58:45 -0300

    Vuex (#26)
    * Added i18n to register
    
    * Finally added i18n
    
    * Added style fixes
    
    * Added regex and validation!
    
    * Fixed values
    
    * added architecture!
    
    * Request added
    
    * Pr fix
    
    * Fixed MERGE
    
    * Style fix
    
    * PR fix
    
    * Pr fix
    
    * Merge fix
    
    * PR fix
    
    * PR FIX
    
    * added login page
    
    * fixed authservice
    
    * Register fix service
    
    * Fixed routing
    
    * Fixed api style
    
    * Fixed links in router-link
    
    * Style Fix
    
    * .env seted
    
    * Now manage actions
    
    * try to fix
    
    * Pipe style fix
    
    * Fixed async
    
    * DEBUGGER DELETED
    
    * Added future file
    
    * Added architecture
    
    * routing fixing
    
    * Fixed view!
    
    * Login logic done
    
    * Auth functionality added
    
    * Added structure
    
    * Added headers
    
    * Added request in auth
    
    * Finally fetch data
    
    * Added files and style
    
    * Added layout style
    
    * Pr Fix
    
    * Added style to layout
    
    * Added navbar layout
    
    * Books are shown
    
    * Debbuger deleted
    
    * Pr Fix
    
    * PR fix
    
    * Added color variables
    
    * PR fix
    
    * Fixed imports
    
    * PR fix
    
    * PR fix
    
    * Pr Fix
    
    * Added books to store
    
    * Added logic
    
    * PR fix
    
    * Indentation fix

--- wolox-books/src/store.js
+++ wolox-books/src/store.js
@@ -1,16 +1,24 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { getBooks } from './services/BookService'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    books: []
   },
   mutations: {
-
+    setBooks (state, { books }) {
+      state.books = books
+    }
   },
   actions: {
-
+    async fetchBooks ({ commit }) {
+      const response = await getBooks()
+      if (response.ok) {
+        commit('setBooks', { books: response.data })
+      }
+    }
   }
 })
--- wolox-books/src/views/Auth.vue
+++ wolox-books/src/views/Auth.vue
@@ -13,17 +13,16 @@
 <script>
 import { localStorageService } from '../services/LocalStorage'
 import { clearHeaders } from '../config/api'
-import { getBooks } from '../services/BookService'
+import { mapState, mapActions } from 'vuex'
 
 import BookList from '@/components/BookList'
 
 export default {
   name: 'Auth',
-  data () {
-    return {
-      books: null
-    }
-  },
+  computed: {
+    ...mapState([
+      'books'
+    ]) },
   components: {
     BookList
   },
@@ -36,12 +35,9 @@ export default {
       clearHeaders()
       this.$router.push('/login')
     },
-    async fetchBooks () {
-      const response = await getBooks()
-      if (response.ok) {
-        this.books = response.data
-      }
-    }
+    ...mapActions([
+      'fetchBooks'
+    ])
   }
 }
 </script>
